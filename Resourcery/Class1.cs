215e8f9a8ca9b2a49f35488fe67308858b48de42
Ryan Roberts <ryansroberts@gmail.com>
2011-12-24 14:34:47 +0000

    first

---
+++ Resourcery/Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Resourcery
+{
+	public class SimplestPossibleModel
+	{
+	}
+
+	public class ConventionResult<T>
+	{
+		public readonly T Result;
+		public readonly bool ConventionMatched;
+
+		protected ConventionResult(T result, bool conventionMatched)
+		{
+			Result = result;
+			ConventionMatched = conventionMatched;
+		}
+
+		public static Func<T1,T2,ConventionResult<T3>> Wrap<T1,T2,T3>(Func<T1,T2,T3> func)
+		{
+			return (t1,t2) => new ConventionResult<T3>(func(t1,t2),true);
+		}
+
+		public static ConventionResult<T> NotMatched()
+		{
+			return new ConventionResult<T>(default(T),false);
+		}
+
+		public static ConventionResult<T> Matched(T result)
+		{
+			return new ConventionResult<T>(result,true);
+		}
+	}
+
+	public class ResourceNameConvention
+	{
+		protected Func<Type, object, ConventionResult<string>> convention;
+
+		public ResourceNameConvention(Func<Type, object, string> convention)
+		{
+			this.convention = ConventionResult<string>.Wrap(convention);
+		}
+
+		public Func<ConventionResult<string>> CurrySubject(Type t, object instance)
+		{
+			return () => convention(t, instance);
+		}
+
+		public static ResourceNameConvention Default
+		{
+			get
+			{
+				return new ResourceNameConvention((t,i) => t.Name);
+			}
+		}
+
+		public void When(Func<Type,object,bool> condition)
+		{
+			convention = (t, i) =>
+			{
+				if (condition(t, i))
+				{
+					return convention(t,i);
+				}
+				return ConventionResult<string>.NotMatched();
+			};
+		}
+	}
+
+	public class ResourceNameConventions
+	{
+		protected IList<ResourceNameConvention> NameConventions = new List<ResourceNameConvention>() 
+			{ ResourceNameConvention.Default };
+
+		public Func<string> Match(Type t,object instance)
+		{
+			return () => NameConventions.Select(c => c.CurrySubject(t, instance)())
+				.First(c => c.ConventionMatched).Result;
+		}
+
+		public ResourceNameConvention Append(Func<Type, object, string> convention)
+		{
+			var nextConvention = new ResourceNameConvention(convention);
+			NameConventions.Insert(0,nextConvention);
+			return nextConvention;
+		}
+	}
+
+	public class Resource : DynamicObject
+	{
+		public class ResourceIntrinsics
+		{
+			readonly Func<string> resourceNameConvention;
+
+			public ResourceIntrinsics(Func<string> resourceNameConvention) {
+				this.resourceNameConvention = resourceNameConvention;
+			}
+
+			public string Name { get { return resourceNameConvention(); } }
+		}
+
+		public ResourceIntrinsics Intrinsics { get; protected set; }
+
+		public Resource(Func<string> nameConvention)
+		{
+			Intrinsics = new ResourceIntrinsics(nameConvention);
+		}
+	}
+
+	public class Resourcery
+	{
+		protected ResourceNameConventions ResourceNameConventions = new ResourceNameConventions();
+
+		public Resource Project<T>(T model)
+		{
+			return new Resource(ResourceNameConventions.Match(typeof(T),model));	
+		}
+
+		public ResourceNameConvention BuildResourceTypeNameUsing(Func<Type, object, string> convention)
+		{
+			return ResourceNameConventions.Append(convention);
+		}
+	}
+}
---
+++ Resourcery/Specs/projecting_empty_model.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace Resourcery.Specs
+{
+	public class with_resourcery
+	{
+		protected static Resourcery resourcery;
+		protected static Resource projection;
+		
+		Establish service =()=> resourcery = new Resourcery();
+
+		protected static void project<T>(T model) { projection =  resourcery.Project(model); }
+	
+		protected static ResourceNameConvention resource_name_convention(Func<Type,object,string> convention)
+		{
+			return resourcery.BuildResourceTypeNameUsing(convention);
+		}
+	}
+	
+	public class projecting_empty_model : with_resourcery
+	{
+		Because of_projecting_empty_model = () => project(new SimplestPossibleModel());
+
+		It has_resource = () => projection.ShouldNotBeNull();
+
+		It resource_name_is_type_name = () => projection.Intrinsics.Name.ShouldEqual(typeof (SimplestPossibleModel).Name);
+	}
+
+	public class resource_name_convention : with_resourcery
+	{
+		Establish context = () => resource_name_convention((t, i) => "test");
+
+		Because of_matching_conventionl = () => project(new SimplestPossibleModel());
+
+		It has_resource_name_from_convention = () => projection.Intrinsics.Name.ShouldEqual("test");
+	}
+
+	public class conditional_resource_name_convention : with_resourcery
+	{
+		Establish context = () => resource_name_convention((t, i) => "test").When((t,i) => false);
+
+		Because of_not_matching_conventionl = () => project(new SimplestPossibleModel());
+
+		It has_resource_name_from_default_rule = () => projection.Intrinsics.Name.ShouldEqual("SimplestPossibleModel");
+	}
+
+}
